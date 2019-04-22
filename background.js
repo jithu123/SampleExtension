332384c2c86dc4a2bc9071275b4691088538b801
jithu123 <jithu123@yahoo.com>
2019-04-21 18:10:37 -0700

    Removing commented code

--- background.js
+++ background.js
@@ -15,8 +15,3 @@ chrome.runtime.onInstalled.addListener(function() {
 });
 
 
-// chrome.runtime.onInstalled.addListener(function() {
-//     chrome.storage.sync.set({color: '#3aa757'}, function() {
-//       console.log("The color is green.");
-//     });
-//   });
